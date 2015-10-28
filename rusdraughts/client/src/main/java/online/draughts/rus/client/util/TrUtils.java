bfbdac5e8260f6c952f3ff5bc5798480395e63eb
Aleksey Popryaduhin <aleksey.popryaduhin@yahoo.com>
2015-10-28 19:45:50 +0300

    Переименование shashki -> draughts.

---
+++ rusdraughts/client/src/main/java/online/draughts/rus/client/util/TrUtils.java
@@ -0,0 +1,37 @@
+package online.shashki.rus.client.util;
+
+import com.google.gwt.core.client.GWT;
+import online.shashki.rus.shared.locale.DraughtsMessages;
+import online.shashki.rus.shared.model.Game;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alekspo
+ * Date: 04.10.15
+ * Time: 13:50
+ */
+public class TrUtils {
+
+  private static DraughtsMessages messages = GWT.create(DraughtsMessages.class);
+
+  public static String translateEndGame(Game.GameEnds gameEnd) {
+    switch (gameEnd) {
+      case WHITE_WIN:
+        return messages.WHITE_WIN();
+      case BLACK_WIN:
+        return messages.BLACK_WIN();
+      case WHITE_LEFT:
+        return messages.WHITE_LEFT();
+      case BLACK_LEFT:
+        return messages.BLACK_LEFT();
+      case DRAW:
+        return messages.DRAW();
+      case SURRENDER_WHITE:
+        return messages.SURRENDER_WHITE();
+      case SURRENDER_BLACK:
+        return messages.SURRENDER_BLACK();
+      default:
+        return "";
+    }
+  }
+}
---
+++ rusdraughts/client/src/main/java/online/draughts/rus/shared/locale/DraughtsMessages.java
@@ -0,0 +1,219 @@
+package online.shashki.rus.shared.locale;
+
+import com.google.gwt.i18n.client.Messages;
+import com.google.gwt.safehtml.shared.SafeHtml;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alekspo
+ * Date: 23.11.14
+ * Time: 20:13
+ */
+public interface DraughtsMessages extends Messages {
+
+  String home();
+
+  String homeToken();
+
+  String play();
+
+  String playToken();
+
+  String unrecognizedPlace();
+
+  String signIn();
+
+  String login();
+
+  String logout();
+
+  String jumbotronGreeting();
+
+  String jumbotronSubGreeting();
+
+  String playTape();
+
+  String playTapeToken();
+
+  String profile();
+
+  String profileToken();
+
+  String myPage();
+
+  String settings();
+
+  String settingsToken();
+
+  String webSocketDoesNotSupport();
+
+  String reconnect();
+
+  String close();
+
+  String error();
+
+  String info();
+
+  String captionGame();
+
+  String next();
+
+  String cancel();
+
+  String chooseYourColor();
+
+  String white();
+
+  String black();
+
+  String waitResponse();
+
+  String yes();
+
+  String no();
+
+  String confirm();
+
+  String inviteMessage(String inviting, String color);
+
+  String inviteToPlay(String opponent, String playType);
+
+  String draughts();
+
+  String atFirstStartPlay();
+
+  String aboutUs();
+
+  String aboutUsToken();
+
+  String profileUpdated();
+
+  String errorWhileProfileUpdate();
+
+  String tooLongPlayerName();
+
+  String playStartDescription();
+
+  String playRestartDescription();
+
+  String invalidCharsInName();
+
+  String tooShortPlayerName();
+
+  String playerRejectedPlayRequest(String sender);
+
+  String failToStartGame();
+
+  String errorWhileGettingProfile();
+
+  String errorWhileGettingGame();
+
+  String selectPlayer();
+
+  String selectAnotherPlayerItsYou();
+
+  String yourTurn();
+
+  String opponentTurn();
+
+  String youDisconnected();
+
+  String whites();
+
+  String blacks();
+
+  String youLose();
+
+  String youWon();
+
+  String playDidNotStart();
+
+  String areYouSureYouWantSurrender();
+
+  String opponentSurrendered();
+
+  String doYouWantToProposeDraw();
+
+  String errorWhileSavingGame();
+
+  String playerProposesDraw(String senderName);
+
+  String playerRejectedDraw(String senderName);
+
+  String doYouWantToCancelMove();
+
+  String playerProposesCancelMove(String publicName);
+
+  String playerRejectedMoveCancel(String publicName);
+
+  String youDontMove();
+
+  String opponentLeftGame();
+
+  String playAlreadyPlaying(String publicName);
+
+  String playViewTitle(String whitePlayer, String blackPlayer, String winner);
+
+  String BLACK_WIN();
+
+  String WHITE_WIN();
+
+  String BLACK_LEFT();
+
+  String WHITE_LEFT();
+
+  String SURRENDER_BLACK();
+
+  String SURRENDER_WHITE();
+
+  String DRAW();
+
+  String contacts();
+
+  String contactsToken();
+
+  String errorToken();
+
+  String myGames();
+
+  String playingTitle();
+
+  String onlineTitle();
+
+  String offlineTitle();
+
+  String errorInAuthentication();
+
+  String opponentNotFound();
+
+  String failToConnectToServer();
+
+  String newGameButtonText();
+
+  String playListButtonText();
+
+  String playerOffline();
+
+  String you();
+
+  String rating(int rating);
+
+  String addToFavorite();
+
+  String removeFromFavorite();
+
+  SafeHtml connectToServer();
+
+  String youHasBeenAddedToFavorite(String publicName);
+
+  String addInFavoriteAllowedOnlyInPlay();
+
+  String youHasBeenRemovedFromFavorite(String publicName);
+
+  String notification();
+
+  String ok();
+
+  String inviteCaption();
+}
